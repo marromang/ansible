8663e7bbd3852e283da51303f4068b0d655059e6
Maria Romero <m.romeroangulo@gmail.com>
2018-02-04 23:28:01 +0100

    Delete init.sh

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-vagrant up
-
-ip1=$(vagrant ssh nodo1 -c "ip address show eth1 | grep 'inet ' | sed -e 's/^.*inet //' -e 's/\/.*$//'")
-ip2=$(vagrant ssh nodo2 -c "ip address show eth1 | grep 'inet ' | sed -e 's/^.*inet //' -e 's/\/.*$//'")
-
-echo $ip1
-echo $ip2
